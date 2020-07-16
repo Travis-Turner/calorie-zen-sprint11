041ca05a4a2ebaaa68f914df489f87fc73e9ecc4
Travis Turner <RoboEnigma@gmail.com>
2020-07-16 20:09:08 +0300

    RR Adding a Route Task 2 Post (corrected)

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Diary from './Diary';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
-      <Diary />
+      <Route exact path="/">
+        <Diary />
+      </Route>
     </BrowserRouter>
   );
 }
