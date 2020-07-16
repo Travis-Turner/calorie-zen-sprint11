18e63a1b6614c18d45f4b252cd8451589d749c56
Travis Turner <RoboEnigma@gmail.com>
2020-07-16 20:18:02 +0300

    RR Working with Multiple Routes Task 1 Pre (corrected)

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Diary from './Diary';
-import Tips from './Tips';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
-      <Route exact path="/">
-        <Diary />
-      </Route>
       <Route path="/">
-        <Tips />
+        <Diary />
       </Route>
     </BrowserRouter>
   );
