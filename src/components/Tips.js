d018a9755d096e953447472842c1dc6b3a2ed177
Travis Turner <RoboEnigma@gmail.com>
2020-07-16 17:54:33 +0300

    RR Adding a Route Task 2 Pre

---
+++ src/components/Tips.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Tips.css';
+
+function Tips() {
+  return (
+    <div>
+      <p>Pretty soon the user can find some daily tips here!</p>
+    </div>
+  );
+}
+
+export default Tips;
