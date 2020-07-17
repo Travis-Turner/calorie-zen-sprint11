3f8a41903afb361f2b75ce37d01c95e1cc25dcab
Travis Turner <RoboEnigma@gmail.com>
2020-07-17 15:00:15 +0300

    RR Links and Navigation Task 3 Post (corrected)

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,7 @@ import { NavLink } from 'react-router-dom';
 function NavBar() {
   return (
     <nav className="menu">
-      <NavLink className="menu__item" activeClassName="menu__item_active" to="/">Home</NavLink>
+      <NavLink className="menu__item" activeClassName="menu__item_active" exact to="/">Home</NavLink>
       <NavLink className="menu__item" activeClassName="menu__item_active" to="/tips">Tips</NavLink>
     </nav>
   );
